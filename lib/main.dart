373883bc4c40a4eb4d8f2d34bc34b3c666a42983
unknown <maharjanaashish01@gmail.com>
2022-02-22 16:50:19 +0545

    fourth commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/pages/homepage.dart';
 import 'package:myapp/pages/loginpage.dart';
 
+
+
 void main() => runApp( const MyApp());
 
 class MyApp extends StatefulWidget {
@@ -18,6 +20,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
   
       themeMode: ThemeMode.dark,
       theme: ThemeData(
@@ -26,8 +29,12 @@ class _MyAppState extends State<MyApp> {
         ),
      // darkTheme: ThemeData(brightness: Brightness.dark),
      routes: {
-       "/": (context) => LoginPage(),
-     } ,
+       "/":(context) => LoginPage(),
+       "/homepage": (context)=> HomePage()
+          
+           
+          
+     },
     );
       
     
--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
-// ignore_for_file: prefer_const_constructors
 
+// ignore_for_file: prefer_const_constructors
 class LoginPage extends StatefulWidget {
   const LoginPage({ Key? key }) : super(key: key);
 
@@ -11,15 +11,17 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String name="";
+  bool chanegebut=false;
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Column(children: [
+      child:SingleChildScrollView( child: Column(children: [
         Image.asset("assets/image/loginimage.png",fit: BoxFit.cover,),
         SizedBox(
           height: 20.0,
         ),
-        Text("Welcome",style: TextStyle(
+        Text("Welcome $name",style: TextStyle(
           fontSize: 24,
           fontWeight: FontWeight.bold,
           
@@ -33,35 +35,75 @@ class _LoginPageState extends State<LoginPage> {
         padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32.0),
         child: Column(
           children: [TextFormField(
-         
+          obscureText: true,
           decoration: InputDecoration(
             
             hintText: "Enter Username",
             labelText: "Username",
           ),
+          onChanged: (value)
+                     {
+                       name=value;
+                       setState(() {});
+                     },
         ),
         TextFormField(
           obscureText: true,
           decoration: InputDecoration(
             hintText: "Enter Password",
             labelText: "Password",
-          ),
+                     ),
+                     
         ),
         ],
         ),
         ),
-        
-        ElevatedButton(
+        SizedBox(
+          height: 20.0,
+        ),
+        InkWell(
+       
+          onTap:() async {
+            setState(() {
+              chanegebut=true;
+            });
+       await Future.delayed(Duration(seconds: 1));
+           Navigator.pushNamed(context, "/homepage");
+            } ,
+         
+          child: AnimatedContainer(
+             duration: Duration(seconds: 1), 
+            height: 40,
+            width: chanegebut? 50 : 150,
+            
+            alignment: Alignment.center,
+            child: chanegebut ?Icon(Icons.done,
+            color: Colors.white,)  :
+            Text("Login",style: TextStyle(
+              
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            
+            ),
+            ),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),color: Colors.deepPurple,),
+            
+          ),
+        )
+       /* ElevatedButton(
         onPressed: (){
-          
+         
           print("hello");
         },
          child: Text("Login"),
          style: TextButton.styleFrom(), 
-         )
+
+         )*/
                 ],
 
           )
-          );
+          ),
+    );
   }
 }
